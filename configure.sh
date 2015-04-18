6f018d977ec31065b4a5ee637190b9663ebfc679
bl4ckh0l3 <bl4ckh0l3z@gmail.com>
2015-04-18 18:28:22 +0200

    Added command for install chilkat lib

--- configure.sh
+++ configure.sh
@@ -6,6 +6,11 @@ virtualenv $DROIDTRAIL_HOME"droidtrail.env"
 source $DROIDTRAIL_HOME"droidtrail.env/bin/activate"
 
 pip install -r requirements.txt
+
+echo "Installing chilkat-lib..."
+cp lib/chilkat-9.5.0-python-2.7-x86_64-linux/_chilkat.so lib/chilkat-9.5.0-python-2.7-x86_64-linux/chilkat.py droidtrail.env/lib/python2.7/site-packages/
+echo "Successfully installed chilkat-lib"
+
 python -c "from droidtrail.dependencies.checkdependencies import CheckDependencies; check_dependecies = CheckDependencies(); check_dependecies.run()"
 
 echo "done"

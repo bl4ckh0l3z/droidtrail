4afae50eab9509b423c4c1a58d80ad226d1f691c
bl4ckh0l3 <bl4ckh0l3z@gmail.com>
2015-04-19 20:30:45 +0200

    Converted in a statis component

--- droidtrail/config/configreader.py
+++ droidtrail/config/configreader.py
@@ -28,62 +28,60 @@ class ConfigReader():
 
     CONFIG_FILE = './droidtrail/config/droidtrail.cfg'
 
-    def __init__(self):
-        global CONFIG_FILE
-        logging.debug("Instantiating the '%s' class" % (self.__class__.__name__))
-        self._config = configparser.ConfigParser()
-        self._config.read(self.__class__.CONFIG_FILE)
-        self._cfg = dict()
-
-    def run(self):
-        if not self._config.has_option('DroidTrail', 'dir_in') \
-                or not self._config.has_option('DroidTrail', 'dir_out') \
-                or not self._config.has_option('DroidTrail', 'white_list_arch_ext'):
+    @staticmethod
+    def run():
+        config = configparser.ConfigParser()
+        config.read(ConfigReader.CONFIG_FILE)
+        cfg = dict()
+        
+        if not config.has_option('DroidTrail', 'dir_in') \
+                or not config.has_option('DroidTrail', 'dir_out') \
+                or not config.has_option('DroidTrail', 'white_list_arch_ext'):
             print("\n### ERROR ### Config element are not defined in the config file\n")
             exit()
         else:
             ### parse the config file
-            logging.debug("Loading config from '%s'" % (self.__class__.CONFIG_FILE))
+            logging.debug("Loading config from '%s'" % (ConfigReader.CONFIG_FILE))
 
             ### parse dir_in config
-            self._cfg['dir_in'] = self._config.get('DroidTrail', 'dir_in')
-            logging.debug("Setting '%s' as input dir" % (self._cfg['dir_in']))
+            cfg['dir_in'] = config.get('DroidTrail', 'dir_in')
+            logging.debug("Setting '%s' as input dir" % (cfg['dir_in']))
 
             ### parse dir_out config
-            self._cfg['dir_out'] = self._config.get('DroidTrail', 'dir_out')
-            logging.debug("Setting '%s' as output dir" % (self._cfg['dir_out']))
+            cfg['dir_out'] = config.get('DroidTrail', 'dir_out')
+            logging.debug("Setting '%s' as output dir" % (cfg['dir_out']))
 
             ### parse white_list_arch_ext config
-            self._cfg['white_list_arch_ext'] = self._config.get('DroidTrail', 'white_list_arch_ext').split(',')
-            logging.debug("Setting '%s' as white listed archive extensions" % (self._cfg['white_list_arch_ext']))
+            cfg['white_list_arch_ext'] = config.get('DroidTrail', 'white_list_arch_ext').split(',')
+            logging.debug("Setting '%s' as white listed archive extensions" % (cfg['white_list_arch_ext']))
 
             ### parse white_list app
-            if self._config.has_option('DroidTrail', 'white_list'):
-                self._cfg['white_list'] = self._config.get('DroidTrail', 'white_list').split(',')
-                logging.debug("Setting '%s' as white listed apps" % (self._cfg['white_list']))
+            if config.has_option('DroidTrail', 'white_list'):
+                cfg['white_list'] = config.get('DroidTrail', 'white_list').split(',')
+                logging.debug("Setting '%s' as white listed apps" % (cfg['white_list']))
             else:
                 logging.debug("White list is not defined")
 
             ### parse black_list app
-            if self._config.has_option('DroidTrail', 'black_list'):
-                self._cfg['black_list'] = self._config.get('DroidTrail', 'black_list').split(',')
-                logging.debug("Setting '%s' as black listed apps" % (self._cfg['black_list']))
+            if config.has_option('DroidTrail', 'black_list'):
+                cfg['black_list'] = config.get('DroidTrail', 'black_list').split(',')
+                logging.debug("Setting '%s' as black listed apps" % (cfg['black_list']))
             else:
                 logging.debug("Black list is not defined")
 
             ### parse passwords config
-            if self._config.has_option('DroidTrail', 'passwords'):
-                self._cfg['passwords'] = self._config.get('DroidTrail', 'passwords').split(',')
-                logging.debug("Setting '%s' as passwords list" % (self._cfg['passwords']))
+            if config.has_option('DroidTrail', 'passwords'):
+                cfg['passwords'] = config.get('DroidTrail', 'passwords').split(',')
+                logging.debug("Setting '%s' as passwords list" % (cfg['passwords']))
             else:
                 logging.debug("Passwords field is not defined")
 
             ### parse proxy config
-            if self._config.has_option('DroidTrail', 'proxy'):
-                self._cfg['proxy'] = self._config.get('DroidTrail', 'proxy')
-                logging.debug("Setting '%s' as proxy" % (self._cfg['proxy']))
+            if config.has_option('DroidTrail', 'proxy'):
+                cfg['proxy'] = config.get('DroidTrail', 'proxy')
+                logging.debug("Setting '%s' as proxy" % (cfg['proxy']))
             else:
                 logging.debug("Proxy is not defined")
 
             logging.debug("######## done")
-        return self._cfg
+        return cfg
